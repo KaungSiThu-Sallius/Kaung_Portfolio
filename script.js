045bd6d8d948c926c4aa9dd9a40f43acb0445710
KaungSiThu-Sallius <kaungsithu.sallius@gmail.com>
2025-04-14 18:46:21 +0530

    fix excel project 2

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let allProjects = [
         { title: 'Employee Turnover (Tableau)', category: 'tableau', imgSrc: 'images/employee_turnover.png', detailPage: 'project_details/Tableau/employee_turnover.html' },
         { title: 'Sleep Disorder (Tableau)', category: 'tableau', imgSrc: 'images/sleep_disorder.png', detailPage: 'project_details/Tableau/sleep_disorder.html' },
-        { title: 'E-Commerce Sales (Excel)', category: 'excel', imgSrc: 'images/e_commerce.png', detailPage: 'project_details/Excel/e_commerce.html' },
+        { title: 'E-Commerce Sales (Excel)', category: 'excel', imgSrc: 'images/ecommerce_dashbaord.png', detailPage: 'project_details/Excel/e_commerce.html' },
         { title: 'Customer Churn (Excel)', category: 'excel', imgSrc: 'images/customer_churn.png', detailPage: 'project_details/Excel/customer_churn.html' },
         { title: 'Video Games Sales (SQL)', category: 'sql', imgSrc: 'images/video_game_sales.png', detailPage: 'project_details/SQL/video_game_sales.html' },
         { title: 'Flight Delays (SQL)', category: 'sql', imgSrc: 'images/flight_delay.png', detailPage: 'project_details/SQL/flight_delay.html' },
